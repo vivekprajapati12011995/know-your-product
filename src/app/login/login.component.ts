e14cf4fd4057084a48b66a52c42fc29f4f4f5be1
vprajapati <vickyprajpati244@yahoo.com>
2017-06-12 16:05:13 +0530

    username and password will be stored into localstorage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,14 +18,26 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.newUser);
-  ///the event that user is created
+    var password = localStorage.getItem(this.newUser.userName);
+    console.log("password1: " + password);
+    console.log("password2: " + this.newUser.password);
+
+
+    if (password == JSON.stringify(this.newUser.password)) {
+      console.log("user logged  in successfully");
+      this.router.navigate(['/']);
+    } else {
+      console.log("user not found");
+      this.router.navigate(['/login']);
+    }
+    ///the event that user is created
     this.userLogged.emit({ user: this.newUser });
     this.newUser = new User();
     this.active = false;
-    setTimeout(() => this.active = true , 0);
-    this.router.navigate(['/']);
+    setTimeout(() => this.active = true, 0);
+
   }
 
 }
